4fbc768e62b522a3ae524e8f854849444c813a9d
Walter Schreppers <walter@sitweb.eu>
2023-06-15 22:08:40 +0200

    cleanup in code

--- random_clocks.py
+++ random_clocks.py
@@ -1,15 +1,25 @@
+# Generated using Chat GPT then heavily tweaked to get something that actually worked.
+# Author: Walter Schreppers
+
 import random
 import math
 from reportlab.pdfgen import canvas
 
+# Create a new PDF document and have some ugly globals ;)
+c = canvas.Canvas("analogue_clocks.pdf")
+hours = []
+minutes = []
+
+
 # Function to draw an analogue clock with given time
 def draw_analogue_clock(c, x, y, hour, minute):
-
+    # Set font and line width
+    c.setFont("Helvetica", 10)
     c.setLineWidth(1) 
+
     # Draw clock face
     c.circle(x, y, 50)
 
-    c.setFont("Helvetica", 10)
     # Draw hour lines
     for i in range(12):
         angle = 2 * math.pi * (i / 12.0)
@@ -43,54 +53,56 @@ def draw_analogue_clock(c, x, y, hour, minute):
     c.setLineWidth(2) 
     c.line(x, y, x + 40 * math.sin(minute_angle), y + 40 * math.cos(minute_angle))
 
-# Create a new PDF document
-c = canvas.Canvas("analogue_clocks.pdf")
 
-# Generate 16 random times and draw the clocks with digital time
-c.setFont("Helvetica", 10)
-hours = []
-minutes = []
-for i in range(16):
-    # Generate random hour (0-23)
-    hour = random.randint(0, 23)
-    hours.append(hour)
+def generate_clocks_with_solutions():
+    # Generate 16 random times and draw the clocks with digital time
+    c.setFont("Helvetica", 10)
 
-    # Generate random minute (round to the nearest multiple of 5)
-    minute = random.randint(0, 11) * 5
-    minutes.append(minute)
+    for i in range(16):
+        # Generate random hour (0-23)
+        hour = random.randint(0, 23)
+        hours.append(hour)
 
-    # Calculate the position for the clock on the page
-    x = 80 + (i % 4) * 150
-    y = 700 - (i // 4) * 180
+        # Generate random minute (round to the nearest multiple of 5)
+        minute = random.randint(0, 11) * 5
+        minutes.append(minute)
 
-    # Draw the analogue clock
-    draw_analogue_clock(c, x, y, hour, minute)
+        # Calculate the position for the clock on the page
+        x = 80 + (i % 4) * 150
+        y = 700 - (i // 4) * 180
 
-    # Draw the time label
-    time_label = "{:02d}:{:02d}".format(hour, minute)
-    c.drawCentredString(x, y - 70, time_label)
+        # Draw the analogue clock
+        draw_analogue_clock(c, x, y, hour, minute)
 
-# Create a new page for clocks without digital time
+        # Draw the time label
+        time_label = "{:02d}:{:02d}".format(hour, minute)
+        c.drawCentredString(x, y - 70, time_label)
+
+
+def generate_clock_exercises():
+    # Generate 16 random times and draw the clocks without digital time
+    for i in range(16):
+        # use same hours as page with the solutions
+        hour = hours[i]
+
+        # use same minute as page with solutions
+        minute = minutes[i]
+
+        # Calculate the position for the clock on the page
+        x = 80 + (i % 4) * 150
+        y = 700 - (i // 4) * 180
+
+        # Draw the analogue clock
+        draw_analogue_clock(c, x, y, hour, minute)
+
+        # Draw a line so you can fill in some text with pen and paper ;)
+        c.drawCentredString(x, y - 70, "_____________")
+
+
+# Generate two pages in pdf, first with solutions, second as exercise
+generate_clocks_with_solutions()
 c.showPage()
-
-# Generate 16 random times and draw the clocks without digital time
-for i in range(16):
-    # Generate random hour (0-23)
-    #hour = random.randint(0, 23)
-    hour = hours[i]
-
-    # Generate random minute (round to the nearest multiple of 5)
-    #minute = random.randint(0, 11) * 5
-    minute = minutes[i]
-
-    # Calculate the position for the clock on the page
-    x = 80 + (i % 4) * 150
-    y = 700 - (i // 4) * 180
-
-    # Draw the analogue clock
-    draw_analogue_clock(c, x, y, hour, minute)
-
-    c.drawCentredString(x, y - 70, "_____________")
+generate_clock_exercises()
 
 # Save the PDF document
 c.save()
